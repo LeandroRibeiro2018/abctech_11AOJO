2e49a52a04f1c15fcf50f606a59a5f538b3a51bc
Leandro Ribeiro <devleandroribeiro@gmail.com>
2023-05-07 19:52:15 -0300

    Correcoes Controllers

--- abctechapi/src/main/java/br/com/fiap/abctechapi/controller/AssistanceController.java
+++ abctechapi/src/main/java/br/com/fiap/abctechapi/controller/AssistanceController.java
@@ -4,6 +4,7 @@ import br.com.fiap.abctechapi.application.AssistanceApplication;
 import br.com.fiap.abctechapi.application.dto.AssistResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ private final AssistanceApplication assistanceApplication;
 public AssistanceController(AssistanceApplication assistanceApplication){
     this.assistanceApplication = assistanceApplication;
 }
-
+@GetMapping
 public ResponseEntity<List<AssistResponseDto>> getAssists(){
     return ResponseEntity.ok(assistanceApplication.getAssists());
 }
--- abctechapi/src/main/java/br/com/fiap/abctechapi/controller/OrderController.java
+++ abctechapi/src/main/java/br/com/fiap/abctechapi/controller/OrderController.java
@@ -4,6 +4,7 @@ import br.com.fiap.abctechapi.application.OrderApplication;
 import br.com.fiap.abctechapi.application.dto.OrderDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class OrderController {
         this.orderApplication = orderApplication;
     }
 
+    @PostMapping
     public ResponseEntity<String> create (@Valid @RequestBody OrderDto orderDto) throws Exception {
         this.orderApplication.createOrder(orderDto);
         return ResponseEntity.ok().build();
